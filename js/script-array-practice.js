071c63c8c30e4e82152cd7d10f7ce49e370cfbb2
Brittany Anderson <142555405+brittanyanderson1@users.noreply.github.com>
2024-12-04 17:43:30 -0800

    updated script-ems.js issues still persist

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -1,33 +1,167 @@
-//STEP 1
+//STEP 1 Create a string array that contains your five favorite movies. Then, use the console to display the second movie in
+// your array.
 
-//STEP 2
+let favoriteMovies = ['Encanto', 'Werewolves Within', 'Shawn of the Dead', 'Idiocracy', 'Tombstone']
 
-//STEP 3
+console.log(favoriteMovies[1]);
 
-//STEP 4
+//STEP 2 Declare an array called movies using the function constructor method. Add the length of 5 into the constructor. Then,
+// assign one of your favorite movies to each index in the array until you have 5 total movies in your array. Then, use the console
+// to display the first movie in your array.
 
-//STEP 5
+//let movies = new Array(5)
 
-//STEP 6
+//movies[0] = 'Encanto'
+//movies[1] = 'Werewolves Within'
+//movies[2] = 'Shaun of the Dead'
+//movies[3] = 'Beetle Juice'
+//movies[4] = 'Tombstone'
 
-//STEP 7
+//console.log(movies[0]);
 
-//STEP 8
 
-//STEP 9
+//STEP 3 Copy your code from step 2. Add a new movie into the 3rd position within your array. Then, use the console to display the
+// length of the array. You should now have 6 total movies stored in the array.
 
-//STEP 10
+//let movies = new Array(5)
 
-//STEP 11
+//movies[0] = 'Encanto'
+//movies[1] = 'Werewolves Within'
+//movies[2] = 'Shaun of the Dead'
+//movies[3] = 'Enola Holmes'
+//movies[4] = 'Beetle Juice'
+//movies[5] = 'Tombstone'
 
-//STEP 12
+//console.log(movies.length);
+//console.log(movies);
 
-//STEP 13
+//STEP 4 Declare an array called movies using literal notation. Then, assign one of your favorite movies to each index in the array
+// until you have 5 total movies in your array. Now, use the delete operator to remove the first movie in the array. Use the console
+// to display the contents of the array.
 
-//STEP 14
+//let movies =  ['Encanto', 'Werewolves Within', 'Shaun of the Dead', 'Enola Holmes', 'Beetle Juice']
 
-//STEP 15
+//delete movies[0]
+//console.log(movies)
 
-//STEP 16
 
-//STEP 17
+
+//STEP 5 Declare an array called movies using literal notation. Then, assign one of your favorite movies to each index in the array
+// until you have 7 total movies in your array. Now, use a for/in loop to iterate through the array and display each movie within the
+// console window.
+
+//let movies = ['Encanto', 'Werewolves Within', 'Shaun of the Dead', 'Enola Holmes', 'Beetle Juice', 'Edward Scissorhands', 'A Quiet Place']
+
+//for(let movie in movies) {
+//    console.log(`${movies[movie]}`)
+//}
+
+//STEP 6 Copy the code from step 5. Now, use a for/of loop to iterate through the array and display each movie within the console
+// window.
+
+//let movies = ['Encanto', 'Werewolves Within', 'Shaun of the Dead', 'Enola Holmes', 'Beetle Juice', 'Edward Scissorhands', 'A Quiet Place']
+
+//for(let movie of movies) {
+//    console.log(`${movie}`)
+//}
+
+//STEP 7 Copy the code from step 5. Using the for/of loop to iterate through the array, display each movie within the console window
+// in a sorted view.
+
+//let movies = ['Encanto', 'Werewolves Within', 'Shaun of the Dead', 'Enola Holmes', 'Beetle Juice', 'Edward Scissorhands', 'A Quiet Place']
+
+//movies.sort()
+
+//for(let movie of movies) {
+//    console.log(movie)
+//}
+
+
+//STEP 8 Copy the code from step 5. Under the existing array, create a new array called leastFavMovies. Populate the array with the
+// 3 movies that you regret watching. Display both arrays within the console window so that it’s formatted to look like this (note 
+//the spacing, you must include that too):
+
+movies = ['Encanto', 'Werewolves Within', 'Shaun of the Dead', 'Enola Holmes', 'Beetle Juice', 'Edward Scissorhands', 'A Quiet Place']; 
+
+const leastFavMovies = ['Sausage Party', 'Amityville Horror', 'Smile']
+
+ console.log("Movies I like:") 
+ console.log("") 
+ for (const movie of movies) { 
+    console.log(movie) 
+} 
+
+console.log("") 
+console.log ("Movies I regret watching:")
+console.log("") 
+for (const movie of leastFavMovies) 
+    { console.log(movie);
+ 
+//for(let movie in movies) {
+//    console.log(`${movies[movie]}`)
+//}
+
+//for(let movie in movies) {
+//    console.log(`${movies[movie]}`)
+//}
+
+//Movies I like:
+
+//Movie 1
+//Movie 2
+//Movie 3
+//…
+
+//Movies I regret watching:
+
+//Movie 1
+//Movie 2
+//Movie 3
+//…
+
+//STEP 9 Copy the code from step 8. Now, use the concat() method to merge the two arrays together into a single array called movies.
+// Use the console window to display the list in reverse sorted
+
+
+//STEP 10 Copy the code from step 9. Use an array function to return just the last item in the array and display it within the console
+// window.
+
+
+//STEP 11 Copy the code from step 10. Remove the previous method and this time use a method to return just the first item in the array
+// and display it within the console window.
+
+
+//STEP 12 Programmatically retrieve the movies in your array that you do not like and return their indices. Then, using those indices,
+// programmatically add movies that you do like.
+
+
+//STEP 13 Create a multi-dimensional array that contains your 5 favorite movies and their ranking from 1-5. The array should look
+// something like this:
+
+//movies = [["Movie 1", 1], ["Movie 2", 2], ["Movie 3", 3], ["Movie 4", 4], ["Movie 5", 5]];
+
+// Now, loop through the array and filter out and display only the movie names. You must use the filter() method and you’ll need to
+// filter out the names by their primitive data type.
+
+
+//STEP 14 Create a string array called employees using literal notation and populate the array with several employee names. Then,
+// create an anonymous function called showEmployee. The function should accept a parameter. Call this function, passing in the employees array into the function as a parameter. Make sure to display the result in the console window. Within the function, loop through the passed in array and display the result so that it looks exactly like this in the console window:
+
+//Employees:
+
+//ZAK
+//JESSICA
+//MARK
+//FRED
+//SALLY
+
+//STEP 15 Write a JavaScript function to filter false, null, 0 and blank values from an array.
+
+//Test Data: console.log(filterValues([58, '', 'abcd', true, null, false, 0]))
+//Expected Result: [58, "abcd", true]
+
+//STEP 16 Write a JavaScript function to get a random item from an array. So if I create a numeric array with 10 numbers and then
+// pass that array into my function, the function should randomly return one of those numbers.
+
+
+//STEP 17 Write a JavaScript function to get the largest number from a numeric array.
--- js/script-ems.js
+++ js/script-ems.js
@@ -1,59 +1,126 @@
-// CREATE AN ARRAY OF EMPLOYEES
+//GLOBAL VARIABLES 
 
+let employees = []
 
-// CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
-// IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
+//CREATE AN ARRAY OF INITIAL EMPLOYEES 
+const initEmployees = [ 
+    {id: 70001234 , name: 'Jack Smith', extension: 1111 , email: 'jsmith@disney.com', department: 'Marketing'},
+    {id: 70001235 , name: 'Ham Burgler', extension: 1112 , email: 'hburglerh@mcdonalds.com', department: 'Sales'},
+    {id: 70001236 , name: 'Homer Simpson', extension: 1113 , email: 'hsimpson@springfieldnuclearplant.com', department: 'Engineering'},
+    {id: 70001237 , name: 'Fraiser Crane', ension: 1114 , email: 'imlistening@Seattle.com', department: 'Quality Assurance'},
+    {id: 70001238 , name: 'Tony Stark', extension: 1115 , email: 'ironman@thedisneycorporation.com', department: 'Executive'} 
+]
 
-
-// GET DOM ELEMENTS
-
-
-// BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
-
-
-// ADD EMPLOYEE
-form.addEventListener('submit', (e) => {
+//CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS function 
+function loadInitEmployees() { 
+    if (localStorage.getItem('employees')) {
+//IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY 
+    employees = JSON.parse(localStorage.getItem('employees')) 
+    } else { 
+// IF NOT, USE INITIAL ARRAY AND SAVE TO STORAGE 
+    employees = initEmployees 
+    localStorage.setItem('employees', JSON.stringify(employees)) 
+    } 
+}
+//GET DOM ELEMENTS 
+const form = document.getElementById('addForm') 
+const empTable = document.getElementById('empTable') 
+const empCount = document.getElementById('empCount')
+//BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS 
+window.onload = function() { 
+    loadInitEmployees() 
+    buildGrid() 
+}
+//ADD EMPLOYEE 
+form.addEventListener('submit', (e) => { 
     // PREVENT FORM SUBMISSION
-
+    e.preventDefault()
     // GET THE VALUES FROM THE TEXT BOXES
+
+    const id = document.getElementById('id').value 
+    const name = document.getElementById('name').value 
+    const extension = document.getElementById('extension').value 
+    const email = document.getElementById('email').value 
+    const department = document.getElementById('department').value
 
     // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
 
+    const newEmployee = { id: Number(id), name, extension: Number(extension), email, department }
+
     // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
+
+    employees.push(newEmployee)
 
     // BUILD THE GRID
 
+    buildGrid()
+
     // RESET THE FORM
+
+    form.reset()
 
     // SET FOCUS BACK TO THE ID TEXT BOX
 
-});
+    document.getElementById('id').focus()
+})
 
-// DELETE EMPLOYEE
-empTable.addEventListener('click', (e) => {
+//DELETE EMPLOYEE
+
+empTable.addEventListener('click', (e) => { 
+    if (e.target.tagName === 'BUTTON') { 
     // CONFIRM THE DELETE
+        if (confirm('Are you sure you want to delete this employee?')) {
 
-        // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
+    // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
 
-        // REMOVE EMPLOYEE FROM ARRAY
+            const id = Number(e.target.parentNode.parentNode.firstElementChild.textContent)
 
-        // BUILD THE GRID
+    // REMOVE EMPLOYEE FROM ARRAY
 
-});
+            employees = employees.filter(employee => employee.id !== id) 
 
-// BUILD THE EMPLOYEES GRID
+    // BUILD THE GRID
+
+            buildGrid() 
+        } 
+    } 
+})
+
+//BUILD THE EMPLOYEES GRID
+
 function buildGrid() {
+
     // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION
 
-    // REBUILD THE TBODY FROM SCRATCH
+    const tbody = empTable.querySelector('tbody'); tbody.innerHTML = ''
+
+    // Clear existing rows
 
     // LOOP THROUGH THE ARRAY OF EMPLOYEES
+
+    for (const employee of employees) {
+
     // REBUILDING THE ROW STRUCTURE
+
+    const row = ` 
+        <tr> 
+            <td>${employee.id}</td> 
+            <td>${employee.name}</td> 
+            <td>${employee.extension}</td> 
+            <td>${employee.email}</td> 
+            <td>${employee.department}</td> 
+            <td><button onclick="deleteEmployee(${employee.id})">Delete</button></td> 
+        </tr> `
 
     // BIND THE TBODY TO THE EMPLOYEE TABLE
 
+    tbody.innerHTML += row; }
+
     // UPDATE EMPLOYEE COUNT
+
+    empCount.textContent = employees.length
 
     // STORE THE ARRAY IN STORAGE
 
-};
+    localStorage.setItem('employees', JSON.stringify(employees)) 
+}
